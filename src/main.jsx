fe803715609ca3f53cb240a57a9eed992a9d5335
Jhon Scarzo <jhon753951@gmail.com>
2025-07-04 17:34:58 +0000

    basic demo

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,28 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import { App } from "./App.jsx";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+let seconds = 0;
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+
+const Counter = () => {
+  const slots = seconds.toString().split("");
+  return (
+    <div style={{ display: "flex", gap: "10px", fontSize: "2rem" }}>
+      {slots.map((digit, index) => (
+        <div key={index} style={{ "border": "solid black 3px", marginRight: "16px", padding: "8px" }}>{digit}</div>
+      ))}
+    </div>
+  );
+};
+
+const render = () => {
+  root.render(<Counter />);
+};
+
+render();
+
+setInterval(() => {
+  seconds += 1;
+  render();
+}, 1000);
